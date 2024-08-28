cfde64c4e6cb64483cf30a1771c2775d8be37a69
Fahad Aziz Khan <fahadkhanofficial2222@gmail.com>
2024-08-28 12:46:50 +0500

    checking animation

--- lib/components/custom_rotated_card.dart
+++ lib/components/custom_rotated_card.dart
@@ -6,34 +6,19 @@ class CustomRotatedCard extends StatefulWidget {
 }
 
 class _CustomRotatedCardState extends State<CustomRotatedCard> {
-  double angle = 0.0;
-  double thisAngle = 0.0;
+  bool isUp = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: GestureDetector(
-        onPanUpdate: (details) {
-          setState(() {
-            angle = details.delta.dx;
-          });
-        },
-        onPanEnd: (details) {
-          setState(() {
-            thisAngle = angle;
-          });
-        },
-        child: Transform(
-          transform: Matrix4.rotationX(thisAngle),
-          child: AnimatedContainer(
-            duration: Duration(seconds: 3),
-            width: 200,
-            height: 200,
-            color: Colors.pink,
-            child: Text('welcom'),
-          ),
+    double bottom = MediaQuery.of(context).size.height * 0.5;
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: 200,
+          height: 200,
+          color: Colors.pink,
         ),
-      ),
-    ));
+      ],
+    );
   }
 }
